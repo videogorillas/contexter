1794289f97e0ec6be8b333fcbd8bc2ed1e0736c1
Sevruk <aleksey.sevruk@gmail.com>
2017-11-18 18:51:41 +0200

    move video capture extension in separate file

---
+++ contexter/VideoCapture.swift
@@ -0,0 +1,101 @@
+//
+//  VideoCapture.swift
+//  contexter
+//
+//  Created by Aleksey Sevruk on 11/18/17.
+//  Copyright © 2017 Aleksey Sevruk. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+import UIKit
+
+extension ViewController:  AVCaptureVideoDataOutputSampleBufferDelegate {
+    func setupAVCapture(){
+        self.session.sessionPreset = AVCaptureSessionPreset640x480
+        guard let device = AVCaptureDevice
+            .defaultDevice(withDeviceType: .builtInWideAngleCamera, mediaType: AVMediaTypeVideo, position: .back)
+            else { return }
+        captureDevice = device
+        //        captureDevice.activeColorSpace = AVCaptureColorSpace.sRGB
+        beginSession()
+    }
+    
+    func beginSession(){
+        var err : NSError? = nil
+        var deviceInput:AVCaptureDeviceInput?
+        do {
+            deviceInput = try AVCaptureDeviceInput(device: captureDevice)
+        } catch let error as NSError {
+            err = error
+            deviceInput = nil
+        }
+        if err != nil {
+            print("error: \(String(describing: err?.localizedDescription))");
+        }
+        if self.session.canAddInput(deviceInput!){
+            self.session.addInput(deviceInput!);
+        }
+        
+        videoDataOutput = AVCaptureVideoDataOutput()
+        videoDataOutput.alwaysDiscardsLateVideoFrames=true
+        videoDataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey: kCVPixelFormatType_32BGRA]
+        
+        videoDataOutputQueue = DispatchQueue(label: "VideoDataOutputQueue")
+        videoDataOutput.setSampleBufferDelegate(self, queue:self.videoDataOutputQueue)
+        videoDataOutput.connection(withMediaType: AVMediaTypeVideo)?.isEnabled = true
+        
+        if self.session.canAddOutput(self.videoDataOutput){
+            self.session.addOutput(self.videoDataOutput)
+        }
+        
+        session.startRunning()
+    }
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!,
+                       didOutputSampleBuffer sampleBuffer: CMSampleBuffer!,
+                       from connection: AVCaptureConnection!) {
+        // Get a CMSampleBuffer's Core Video image buffer for the media data
+        let  imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+        // Lock the base address of the pixel buffer
+        CVPixelBufferLockBaseAddress(imageBuffer!, CVPixelBufferLockFlags.readOnly);
+        
+        
+        // Get the number of bytes per row for the pixel buffer
+        let baseAddress = CVPixelBufferGetBaseAddress(imageBuffer!);
+        
+        // Get the number of bytes per row for the pixel buffer
+        let bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer!);
+        // Get the pixel buffer width and height
+        let width = CVPixelBufferGetWidth(imageBuffer!);
+        let height = CVPixelBufferGetHeight(imageBuffer!);
+        
+        // Create a device-dependent RGB color space
+        let colorSpace = CGColorSpaceCreateDeviceRGB();
+        
+        // Create a bitmap graphics context with the sample buffer data
+        var bitmapInfo: UInt32 = CGBitmapInfo.byteOrder32Little.rawValue
+        bitmapInfo |= CGImageAlphaInfo.premultipliedFirst.rawValue & CGBitmapInfo.alphaInfoMask.rawValue
+        //let bitmapInfo: UInt32 = CGBitmapInfo.alphaInfoMask.rawValue
+        let context = CGContext.init(data: baseAddress, width: width, height: height, bitsPerComponent: 8, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo)
+        // Create a Quartz image from the pixel data in the bitmap graphics context
+        let quartzImage = context?.makeImage();
+        // Unlock the pixel buffer
+        CVPixelBufferUnlockBaseAddress(imageBuffer!, CVPixelBufferLockFlags.readOnly);
+        
+        if quartzImage != nil {
+            // Create an image object from the Quartz image
+            let image = UIImage.init(cgImage: quartzImage!);
+            imageSubject.onNext(image)
+        }
+    }
+    
+    // clean up AVCapture
+    func stopCamera(){
+        session.stopRunning()
+    }
+    
+    func startCamera() {
+        session.startRunning()
+    }
+}
