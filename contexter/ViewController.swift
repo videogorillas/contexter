f78a8707a9c3803a35143e63df804a0b322f6ec2
Sevruk <aleksey.sevruk@gmail.com>
2017-11-18 02:02:43 +0200

    initial + UI

---
+++ contexter/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  contexter
+//
+//  Created by Aleksey Sevruk on 11/18/17.
+//  Copyright © 2017 Aleksey Sevruk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
+    @IBOutlet weak var label5: UILabel!
+    
+    @IBAction func onDetectButton(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
